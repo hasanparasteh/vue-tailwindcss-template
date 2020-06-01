915db654e388f46d2ec7f58604084d68200c1050
ashtadsam <ashtadsam@gmail.com>
2020-06-01 15:11:03 +0430

    init

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import "@/assets/css/tailwind.css";
+
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img class="w-40 mx-auto" alt="Tailwindcss logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
